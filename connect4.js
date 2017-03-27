f6e10c807214783aa37bc9d1928d3967fd2b2dc8
589865 <stanleygorman@C02T44V8H03Q.home>
2017-03-27 00:54:19 -0400

    css game board working with user limitation

--- connect4.js
+++ connect4.js
@@ -1,38 +1,45 @@
 $(function(){
 
-$canvas = $("canvas")
-red = 'rgb(225,0,0)';
-black = 'rgb(0,0,0)';
-var count = 1;
+  $canvas = $("canvas")
+  red = 'rgb(225,0,0)';
+  black = 'rgb(0,0,0)';
+  var count = 1;
 
 
-var createCircle = function(color){
-index = $canvas_number;
-circle = $canvas[index].getContext('2d');
-circle.beginPath();
-circle.arc(50, 50, 50, 0, 2 * Math.PI, false);
-circle.fillStyle = color;
-circle.fill();
-}
-
-
-var placeChip = function(){
-
-
-  $canvas_clicked =$(this).attr('id');
-  console.log($canvas_clicked);
-  $canvas_number = $canvas_clicked - 1;
-  $canvas_above_id = $(this).attr('id') - 7;
-  canvas_above = $(this).parent().parent().prev().find('#' + $canvas_above_id);
-
-  if ($canvas_clicked >= 43 || $(this).hasClass('can_place')){
-    $(this).attr('class', 'has_chip');
-    canvas_above.attr('class', 'can_place');
-    createCircle(red)
+  var createCircle = function(color){
+    index = $canvas_number;
+    circle = $canvas[index].getContext('2d');
+    circle.beginPath();
+    circle.arc(50, 50, 40, 0, 2 * Math.PI, false);
+    circle.fillStyle = color;
+    circle.fill();
   }
 
 
+  var placeChip = function(){
+    count++;
+
+
+    $canvas_clicked =$(this).attr('id');
+    console.log($canvas_clicked);
+    $canvas_number = $canvas_clicked - 1;
+    $canvas_above_id = $(this).attr('id') - 7;
+    canvas_above = $(this).parent().parent().prev().find('#' + $canvas_above_id);
+    if ($canvas_clicked % 2 == 1){
+      if ($canvas_clicked >= 43 || $(this).hasClass('can_place')){
+        $(this).attr('class', 'has_chip');
+        canvas_above.attr('class', 'can_place');
+        createCircle(red);
+      }
+    }
+    else {
+      if ($canvas_clicked >= 43 || $(this).hasClass('can_place')){
+        $(this).attr('class', 'has_chip');
+        canvas_above.attr('class', 'can_place');
+        createCircle(black);
+}}
   }
-$("canvas").on("click", placeChip)
+
+  $("canvas").on("click", placeChip)
 
 })
