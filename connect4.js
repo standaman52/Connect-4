870d14ff5cfe14e918c358a021288186149f0340
589865 <stanleygorman@C02T44V8H03Q.home>
2017-03-29 00:06:33 -0400

    deleting

--- connect4.js
+++ connect4.js
@@ -7,17 +7,7 @@ $(function(){
   toggle = false;
   var  redArray =[];
   var blueArray = [];
-  var winCases = [  // this object contains a list of all winning combinations
-    // by downward diagonal
-    ['1', '9', '17', '25'],
-    ['2', '10', '18', '26'],
-    ['3', '11', '19', '27'],
-    ['4', '12', '20', '28'],
-    ['8', '16', '24', '32'],
-    ['9', '17', '25', '33'],
-    ['10', '18', '26', '34'],
-    ['11', '19', '27', '35'],
-    ['15', '23', '31', '39'],
+  var winCases = [  // this object contains a list of all winning
     // by row
     ['1', '2', '3', '4'],
     ['2', '3', '4', '5'],
@@ -31,7 +21,6 @@ $(function(){
     ['30', '31', '32', '33'],
     ['36', '37', '38', '39'],
     ['37', '38', '39', '40'],
-
     ['3', '4', '5', '6'],
     ['4', '5', '6', '7'],
     ['10', '11', '12', '13'],
@@ -43,31 +32,8 @@ $(function(){
     ['31', '32', '33', '34'],
     ['32', '33', '34', '35'],
     ['38', '39', '40', '41'],
-    ['39', '40', '41', '42'],
-    // by column
-    ['1', '8', '15', '22'],
-    ['2', '9', '16', '23'],
-    ['3', '10', '17', '24'],
-    ['4', '11', '18', '25'],
-    ['5', '12', '19', '26'],
-    ['6', '13', '20', '27'],
-    ['7', '14', '21', '28'],
+    ['39', '40', '41', '42']
 
-    ['8', '15', '22', '29'],
-    ['9', '16', '23', '30'],
-    ['10', '17', '24', '31'],
-    ['11', '18', '25', '32'],
-    ['12', '19', '26', '33'],
-    ['13', '20', '27', '24'],
-    ['14', '21', '28', '35'],
-
-    ['15', '22', '29', '36'],
-    ['16', '23', '30', '37'],
-    ['17', '24', '31', '38'],
-    ['18', '25', '32', '39'],
-    ['19', '26', '33', '40'],
-    ['20', '27', '34', '41'],
-    ['21', '28', '35', '42'],
   ];
 
 
@@ -83,23 +49,25 @@ $(function(){
 
 
 
-    var fourInArow = 0;
-  var checkWin = function(){
-    for (var i = 0; i < winCases.length; i++) {
-      for (var j = 0; j < winCases[i].length; j++) {
-        for (var m = 0; m < redArray.length; m++) {
-          if(  winCases[i][j] === redArray[m]   )
-          {
 
-            console.log(winCases[i][j] +"WINCASES");
-            console.log(redArray[m]);
+  var checkWin = function(array){
+
+    for (var i = 0; i < winCases.length; i++) {
+        var fourInArow = 0;
+      for (var j = 0; j < winCases[i].length; j++) {
+        for (var m = 0; m < array.length; m++) {
+          if(  winCases[i][j] === array[m]   )
+          {
+                    fourInArow++;
+            console.log(winCases[i][j] +" WINCASES");
+            console.log(array[m]);
             console.log("------------------------");
 
 
           }
 
         }
-        if(count > 5 ){
+        if(fourInArow == 4 ){
           console.log("u win");
           return;
         }
@@ -125,11 +93,15 @@ $(function(){
         $(this).off();
         toggle = true;
         redArray.push($canvas_clicked);
-        redArray.sort()
+        console.log($canvas_clicked);
+        // redArray.sort()
         console.log(redArray);
         count++;
-          if(count > 3 )
-         checkWin();
+       if (count >6) {
+             checkWin(redArray);
+       }
+
+
       }
     }
     else{
@@ -140,11 +112,15 @@ $(function(){
         $(this).off()
         toggle = false;
         blueArray.push($canvas_clicked);
-        blueArray.sort()
+        // blueArray.sort()
         console.log(blueArray);
+        count++;
+       if (count >6) {
+             checkWin(blueArray);
+       }
 
 
-        // checkWin();
+
       }
     }
 
